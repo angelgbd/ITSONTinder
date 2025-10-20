eaf79e4acaf581a0b460f5a183317c7bf7fe255c
angelgbd <baeltranangel@gmail.com>
2025-10-19 22:00:01 -0700

    Se añadio el Mapeo con JPA

---
+++ ITSONTinder/src/main/java/com/example/itsontinder/ITSONTinder.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.example.itsontinder;
+
+/**
+ *
+ * @author angel
+ */
+public class ITSONTinder {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
